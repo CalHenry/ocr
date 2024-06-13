d8caa5d51bf8161e82795f96d718b195a5eea9e0
C0urne <chckt316@etik.com>
2024-06-13 21:52:55 +0200

    13/06 addin comments of function split and process ocr

--- code/functions/split_and_process_ocr_text.R
+++ code/functions/split_and_process_ocr_text.R
@@ -20,7 +20,7 @@ split_and_process_ocr_text <- function(text) {
     mutate(content = str_replace_all(content, "\\s*(\\d{1,3}[.,]\\d{1,3}[^\\|]*)", function(x) {
       return(paste0("|", x, "|"))
     })) %>%
-    mutate(contents = 
+    mutate(content = 
              {
                if (any(str_detect(content, "(\\d{1,3}[.,]{1}\\d{1,3})"))) {
                  str_replace_all(content, "\\s*(\\d{1,3}[.,]\\d{1,3})", "\\|\\0\\|")
@@ -28,8 +28,8 @@ split_and_process_ocr_text <- function(text) {
                  str_replace_all(content, "\\d{1,3}\\s+", "\\|\\0\\|")
                }
              }) %>% 
-    mutate(contentss = str_replace_all(contents, "\\|\\s*\\|*", "|")) %>% 
-    mutate(arg = str_split(contentss, "(?<!\\d)\\.+(?!\\d)")) %>% 
+    mutate(content = str_replace_all(content, "\\|\\s*\\|*", "|")) %>% 
+    mutate(arg = str_split(content, "(?<!\\d)\\.+(?!\\d)")) %>% 
     mutate(arg = as.character(arg)) %>% 
     mutate(arg = str_replace_all(arg, "\", \"", "")) %>%
     mutate(arg = str_replace_all(arg, "^c\\(\"|\"\\)$", "")) %>% 
@@ -38,7 +38,7 @@ split_and_process_ocr_text <- function(text) {
   return(processed_dataset)
 }
 
-# Breakdown of the function -------------------------------------------------
+# ----------------------- Breakdown of the function ---------------------------
 
 # processed_dataset <- tibble(text = unlist(strsplit(text, "\n"))) %>%
 #   split the text by new lines "\n", then turn the list of these texts to dataset.
@@ -80,7 +80,7 @@ split_and_process_ocr_text <- function(text) {
 #     return(paste0(str_extract(x, "([^a-zA-Z])"), "(", str_extract(x, "(La|Le|L')"), ")", str_extract(x, "([^a-zA-Z])")))
 #   }
 # }))
-# replaces any occurences of [any alpha symbol]("La" or "Le" or "L'")[any alpha symbol]
+# replaces any occurrences of [any alpha symbol]("La" or "Le" or "L'")[any alpha symbol]
 # by itself if the string is == NA or has parenthesis around a word
 # by a reconstructed matched text by extracting the non-alphabetic characters before and after the "La", "Le", or "L'"
 # and inserting the "La", "Le", or "L'" between them.
@@ -100,58 +100,35 @@ split_and_process_ocr_text <- function(text) {
 #              str_replace_all(content, "\\d{1,3}\\s+", "\\|\\0\\|")
 #            }
 #          })
-# 
 
+# This code find digits and enclose them into pipes.
+# We use a "if" statement because we differentiate 2 cases:
+# when the number has decimals and when it doesn't.
+# "if" statement correspond to number with digits (123,456)
+# "else" statement are the other numbers
+# the match is returned enclosed into pipes |123,456|
 
+# mutate(content = str_replace_all(content, "\\|\\s*\\|*", "|")) %>%
+# replace any sequence of pipes (including pipes with whitespaces inbetween) by a single pipe
+# "||  |||" --> "|"
 
+# mutate(arg = str_split(content, "(?<!\\d)\\.+(?!\\d)")) %>% 
+# is used to keep the relevant content of each string.
+# split the string based on a pattern that catch sequence of 1 or more dots.
+# output is a vector containing all that was in between the sequences of dots
+# "Bert....................| Houille..............| 1,337 62.125 361|"
+# --> "Bert" "| Houille" "| 1,337 62.125 361|"
 
+# mutate(arg = as.character(arg)) %>% 
+# turn to character class
 
-# Breakdown of the function -------------------------------------------------
+# mutate(arg = str_replace_all(arg, "\", \"", "")) %>%
+# mutate(arg = str_replace_all(arg, "^c\\(\"|\"\\)$", "")) %>% 
+# These lines are used to remove the c("   "), that appears after converting to char class
+# first line remove the "
+# second line removes the "c(" and the ")"
 
-# processed_dataset <- tibble(text = unlist(strsplit(text, "\n"))) %>%
-#   split the text by new lines "\n", then turn the list of these texts to dataset.
-#   Getting ready for all the text cleanning.
+# separate_wider_delim(arg, delim = "|", names_sep = "", too_few = "align_start")
+# cut the arg var into pieces based on the delimiter "|"
+# we obtain the final form of the dataset with the "arg" split for each distinct element of the string.
 
-#  mutate(content = str_replace(value, "\\.\\s+\\.", "..")) %>%:
-# remove whitespaces in between sequences of periods ".. ... -> ....."
-
-#  mutate(content = str_replace(content, "^(\\p{L})\\s", "")) %>%
-#   Remove alone leading letter followed by whitespace at beg of strings.
-
-#  mutate(content = str_replace_all(content, "\\[|\\]|\\}|\\{|\\!", "|")) %>%:
-#   Replace what is supposed to be "|" with "|".
-
-#  mutate(content = str_replace(content, "^\\s*\\|", "")) %>%:
-#   Remove leading "|".
-
-#  mutate(content = str_replace(content, "\\|\\|", "|")) %>%:
-#   Replace "||" with "|".
-
-# mutate(content = str_replace(content, "(?<=\\.\\.)1", "|")) %>%:
-#   Replace the "1" that have consecutive dots ahead with "|".
-# The ?<=\\.. is a positive lookbehind assertion that checks if there is a period before the "1".
-
-#  mutate(content = str_replace_all(content, "(\\.{2,})\\s*([A-Za-z0-9])", "\\1|\\2")) %>%
-#   Replace consecutive dots followed by alphanumeric character with "|".
-#   iow: We add "|" if it is not
-
-#  mutate(content = str_trim(content, side = "left")) %>%
-#   Trim leading whitespace.
-
-#  mutate(content = str_replace(content, "(.)(dem)", "i\\2")) %>%
-#   Replace "dem" preceded by any character with "idem".
-
-#  mutate(arg = str_split(content, "(?<!\\d)\\.+(?!\\d)")) %>%
-#   Split `content` column by non-digit surrounded by dots.
-
-#  mutate(arg = as.character(arg)) %>%
-#   Convert `arg` column to character.
-
-#  mutate(arg = str_replace_all(arg, "\", \"", "")) %>%
-#   Remove "\", and "\", " from `arg` column.
-
-#  mutate(arg = lapply(arg, function(x) str_replace_all(x, "^c\\(\"|\"\\)$", ""))) %>%
-#   Remove leading "c(" and trailing ")" from `arg` column.
-
-#  separate_wider_delim(arg, delim = "|", names_sep = "", too_few = "align_start")
-# Separate `arg` column into multiple columns using "|" as delimiter.
--- code/master.R
+++ code/master.R
@@ -19,5 +19,3 @@ source("code/functions/image_processing_function_even.R")
 source("code/functions/image_processing_function_odd.R")
 source("code/functions/split_and_process_ocr_text.R")
 
-source("code/functions/split_and_process_ocr_text - Copie.R")
-
