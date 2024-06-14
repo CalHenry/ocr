d6310679f82f7558bab4e4bd800e10f293c41f84
C0urne <chckt316@etik.com>
2024-06-14 20:20:13 +0200

    remove unneed function

--- code/functions/na_if_high_punctuation.R
+++
@@ -1,8 +0,0 @@
-na_if_high_punctuation <- function(str) {
-  punctuation_ratio <- str_count(str, "[:punct:]") / str_length(str)
-  if (punctuation_ratio > 0.8) {
-    return(NA)
-  } else {
-    return(str)
-  }
-}
